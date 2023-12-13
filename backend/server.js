4dca35635741ae69316fd0db1a58936a2bcb187c
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-12-13 23:09:00 +0530

    create server

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const app = require("./app");
+
+//Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
+
+//Setting up config file
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
+}
+
+//create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
+
+//Handling Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down the server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
